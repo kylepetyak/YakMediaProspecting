89d1ec83412e60e11e0058990f77a6e8f249f19a
kylepetyak <kyle@yak.media>
2025-10-23 10:21:17 -0700

    Update CreateUserPage.tsx

--- src/components/pages/CreateUserPage.tsx
+++ src/components/pages/CreateUserPage.tsx
@@ -169,7 +169,7 @@ export function CreateUserPage() {
               <div className="space-y-2">
                 <div className="flex items-center gap-2">
                   <code className="flex-1 bg-slate-100 px-3 py-2 rounded text-sm">
-                    https://success.yak.media/#/login
+                    https://success.yak.media/login
                   </code>
                   <Button
                     variant="outline"
